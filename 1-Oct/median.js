65691883f6484bcb4862c1de5a6e86256d030b25
Kiran <kiran.shet77@gmail.com>
2022-10-26 14:01:08 +0530

    Problems on JS

---
+++ 1-Oct/median.js
@@ -0,0 +1,50 @@
+function getMedian(left, right){
+    let leftIndex = 0, rightIndex = 0, mergedIndex = 0;
+    let leftArrLength = left.length,
+        rightArrLength = right.length;
+    let merged = [];
+    while(mergedIndex < (leftArrLength + rightArrLength)){
+        if(left[leftIndex] <= right[rightIndex]) {
+            merged[mergedIndex] = left[leftIndex];
+            leftIndex++;
+        } else {
+            merged[mergedIndex] =right[rightIndex];
+            rightIndex++;
+        }
+
+        mergedIndex++;
+    }
+
+        console.log(merged)
+    return (leftArrLength + rightArrLength) % 2 == 0  
+                ? (merged[mergedIndex - 1] / 2 + merged[mergedIndex - 1]/2 - 1)/2
+                : (merged[mergedIndex - 1])/2;
+}
+
+function getMedian2(left,right){
+    let leftIndex = 0, rightIndex = 0;
+    let leftArrLength = left.length;
+    let m1 = -1, m2 = -1;
+   for(let count = 0; count <= leftArrLength; count++){
+        if(left[leftIndex] <= right[rightIndex]) {
+            m1 = m2;
+            m2 = left[leftIndex];
+            leftIndex++;
+        } else {
+            m1 = m2;
+            m2 = right[rightIndex]; 
+            rightIndex++;
+        }
+    }
+    console.log(m1 + " " + m2)
+    return (m2 + m1) /2;
+}
+
+
+
+var left = [11, 12, 25, 26, 38];
+var right = [2, 23, 17, 20, 45];
+
+
+console.log(getMedian(left, right));
+//console.log(getMedian2(left, right));
